1ab35a7e834d8d9ae02039d5f9e5618648bd4526
chaitanya44444 <93338022+chaitanya44444@users.noreply.github.com>
2022-11-15 16:07:04 +0530

    Create fhghfgh.py

---
+++ fhghfgh.py
@@ -0,0 +1,5 @@
+num=int(input("write a number here to see if it is divisible by 3 and 2 "))
+if n%6 == 0:
+    print("it is divisible by both 3 and 2")
+if n%6 ==1:
+    print("not divisible by both 3 and 2")
